85e549720b1feb8f32c8249ac338376f016eb42a
Cristian Marcel <lcristianiim@yahoo.com>
2017-07-01 16:32:44 +0300

    Selection Sort, Bubble Sort

--- src/main/java/com/common/algorithms/SortAlgorithms.java
+++ src/main/java/com/common/algorithms/SortAlgorithms.java
@@ -1,12 +1,40 @@
 package com.common.algorithms;
 
+import java.time.Instant;
 import java.util.Arrays;
 
+import static com.common.algorithms.Utilities.exchange;
+import static com.common.algorithms.Utilities.less;
+
 public class SortAlgorithms {
+
     public static int[] selectionSort(int[] array) {
 
-        
+        for (int i = 0; i < array.length; i++) {
+            int minPosition = i;
+
+            for(int j = i + 1; j < array.length; j++) {
+                if (less(array[j], array[minPosition]) ) {
+                    minPosition = j;
+                }
+            }
+            exchange(array, i, minPosition);
+        }
 
        return array;
+    }
+
+    public static int[] bubbleSort(int[] array) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+
+            for (int j = 0; j < array.length - 1; j++) {
+                    if(less(array[j+1], array[j])) {
+                        exchange(array, j+1, j);
+                    }
+                }
+
+            }
+        return array;
     }
 }
